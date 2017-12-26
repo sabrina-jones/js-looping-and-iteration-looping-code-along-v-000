682ba54fc6616d01a67e3cfaad1b895aa28f8e84
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-26 23:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+     console.log(`Welcome ${names[i]}! You are employee #${[i+1]}.`);
+}
+ return names;
+}
+
+
+
+function tailsNeverFails(){
+  let tails = Math.random() >= 0.5
+  let counter = 0;
+
+  while (tails) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`
+}
