9be09dec4a1f37ad7d755fcb630876ce450bd401
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-26 23:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function printBadges(names) {
-  for (let i = 0; i < names.length; i++) {
-     console.log(`Welcome ${names[i]}! You are employee #${[i+1]}.`);
+
+
+function tailsNeverFails(){
+  let tails = Math.random() >= 0.5
+  let counter = 0;
+
+  while (tails) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`
 }
- return names;
-}
-
-
-
